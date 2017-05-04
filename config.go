d0a71df0f6bf0318e3bbe5d5aa7eb505699ce86d
Sam H <sam@Sam.local>
2017-05-05 00:39:11 +0800

    added config

--- config.go
+++ config.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+const (
+	settingFile      = "setting.json"
+	siteTemplateFile = "account.key"
+)
+
+var configDir string
+var siteConfDir string
+var siteRootDir string
+
+func init() {
+	configDir = os.Getenv("NGX_CLI_CONFIG")
+	siteConfDir = os.Getenv("NGX_CLI_SITE_CONFIG")
+	siteRootDir = os.Getenv("NGX_CLI_SITE_ROOT")
+
+	if configDir == "" {
+
+		if u, err := user.Current(); err == nil {
+			configDir = filepath.Join(u.HomeDir, ".config", "nginx")
+		}
+	}
+
+	if siteConfDir == "" {
+		siteConfDir = "/etc/nginx/conf.d"
+	}
+
+	if siteRootDir == "" {
+		siteRootDir = "/opt/local/www"
+	}
+}
+
+type userConfig struct {
+}
+
+func readConfig() (*userConfig, error) {
+	b, err := ioutil.ReadFile(filepath.Join(configDir, settingFile))
+	if err != nil {
+		return nil, err
+	}
+	uc := &userConfig{}
+	if err := json.Unmarshal(b, uc); err != nil {
+		return nil, err
+	}
+
+	return uc, nil
+}
+
+func writeConfig(uc *userConfig) error {
+	b, err := json.MarshalIndent(uc, "", "  ")
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(configDir, 0700); err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filepath.Join(configDir, settingFile), b, 0600)
+}
